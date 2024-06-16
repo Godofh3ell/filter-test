05397eb6f6f381975190f83edfb907402f6e7751
Godofh3ell <66544811+Godofh3ell@users.noreply.github.com>
2024-06-17 01:31:30 +0530

    Update render_template.py

--- web/utils/render_template.py
+++ web/utils/render_template.py
@@ -9,8 +9,27 @@ import aiofiles
 import logging
 import aiohttp
 
+import urllib.parse
+import aiofiles
 
-async def media_watch(message_id):
+PASSWORD = "your_secure_password"  # Define your password here
+
+async def media_watch(message_id, provided_password=None):
+    if provided_password != PASSWORD:
+        return '''
+            <html>
+                <head><title>Password Required</title></head>
+                <body>
+                    <h1>Enter Password to Access the File</h1>
+                    <form method="get">
+                        <input type="password" name="password" required />
+                        <input type="hidden" name="message_id" value="{}" />
+                        <button type="submit">Submit</button>
+                    </form>
+                </body>
+            </html>
+        '''.format(message_id)
+    
     media_msg = await temp.BOT.get_messages(BIN_CHANNEL, message_id)
     file_properties = await TGCustomYield().generate_file_properties(media_msg)
     file_name, mime_type = file_properties.file_name, file_properties.mime_type
@@ -21,5 +40,6 @@ async def media_watch(message_id):
             heading = 'Watch - {}'.format(file_name)
             html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
     else:
-        html = '<h1>This is not streamable file</h1>'
+        html = '<h1>This is not a streamable file</h1>'
     return html
+    
