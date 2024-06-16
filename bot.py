0bfba2cb1d9149bce60c9627dfc14ef07d7b9d04
Godofh3ell <66544811+Godofh3ell@users.noreply.github.com>
2024-06-17 01:29:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,23 @@ from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 import time, os, platform
 from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
+from aiohttp import web
+import urllib.parse
+import aiofiles
+from web.utils.render_template import media_watch
 
+async def handle_request(request):
+    message_id = request.query.get('message_id')
+    provided_password = request.query.get('password')
+    html_content = await media_watch(message_id, provided_password)
+    return web.Response(text=html_content, content_type='text/html')
+
+app = web.Application()
+app.router.add_get('/media', handle_request)
+
+if __name__ == '__main__':
+    web.run_app(app, port=8080)
+    
 
 class Bot(Client):
     def __init__(self):
