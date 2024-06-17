033d5c44137ae8395266e542c02dd3222c13a5c1
Godofh3ell <66544811+Godofh3ell@users.noreply.github.com>
2024-06-17 22:01:17 +0530

    Update custom_dl.py

--- web/utils/custom_dl.py
+++ web/utils/custom_dl.py
@@ -1,47 +1,40 @@
 import math
 from typing import Union
 from pyrogram.types import Message
-from utils import temp
-from pyrogram import Client, utils, raw
+from pyrogram import Client, raw
 from pyrogram.session import Session, Auth
 from pyrogram.errors import AuthBytesInvalid
 from pyrogram.file_id import FileId, FileType, ThumbnailSource
 
-
 async def chunk_size(length):
     return 2 ** max(min(math.ceil(math.log2(length / 1024)), 10), 2) * 1024
-
 
 async def offset_fix(offset, chunksize):
     offset -= offset % chunksize
     return offset
 
-
 class TGCustomYield:
-    def __init__(self):
-        """ A custom method to stream files from telegram.
-        functions:
-            generate_file_properties: returns the properties for a media on a specific message contained in FileId class.
-            generate_media_session: returns the media session for the DC that contains the media file on the message.
-            yield_file: yield a file from telegram servers for streaming.
-        """
-        self.main_bot = temp.BOT
+    def __init__(self, bot: Client):
+        self.main_bot = bot
 
-    @staticmethod
-    async def generate_file_properties(msg: Message):
-        media = getattr(msg, msg.media.value, None)
+    async def generate_file_properties(self, msg: Message):
+        if msg.video:
+            media = msg.video
+        elif msg.document:
+            media = msg.document
+        else:
+            raise ValueError("Message does not contain video or document")
+
         file_id_obj = FileId.decode(media.file_id)
 
-        # The below lines are added to avoid a break in routes.py
-        setattr(file_id_obj, "file_size", getattr(media, "file_size", 0))
-        setattr(file_id_obj, "mime_type", getattr(media, "mime_type", ""))
-        setattr(file_id_obj, "file_name", getattr(media, "file_name", ""))
+        setattr(file_id_obj, "file_size", media.file_size)
+        setattr(file_id_obj, "mime_type", media.mime_type)
+        setattr(file_id_obj, "file_name", media.file_name)
 
         return file_id_obj
 
     async def generate_media_session(self, client: Client, msg: Message):
         data = await self.generate_file_properties(msg)
-
         media_session = client.media_sessions.get(data.dc_id, None)
 
         if media_session is None:
@@ -84,52 +77,30 @@ class TGCustomYield:
 
         return media_session
 
-    @staticmethod
-    async def get_location(file_id: FileId):
+    async def get_location(self, file_id: FileId):
         file_type = file_id.file_type
 
-        if file_type == FileType.CHAT_PHOTO:
-            if file_id.chat_id > 0:
-                peer = raw.types.InputPeerUser(
-                    user_id=file_id.chat_id,
-                    access_hash=file_id.chat_access_hash
-                )
-            else:
-                if file_id.chat_access_hash == 0:
-                    peer = raw.types.InputPeerChat(
-                        chat_id=-file_id.chat_id
-                    )
-                else:
-                    peer = raw.types.InputPeerChannel(
-                        channel_id=utils.get_channel_id(file_id.chat_id),
-                        access_hash=file_id.chat_access_hash
-                    )
-
-            location = raw.types.InputPeerPhotoFileLocation(
-                peer=peer,
-                volume_id=file_id.volume_id,
-                local_id=file_id.local_id,
-                big=file_id.thumbnail_source == ThumbnailSource.CHAT_PHOTO_BIG
-            )
-        elif file_type == FileType.PHOTO:
-            location = raw.types.InputPhotoFileLocation(
+        if file_type == FileType.VIDEO:
+            location = raw.types.InputVideoFileLocation(
                 id=file_id.media_id,
                 access_hash=file_id.access_hash,
                 file_reference=file_id.file_reference,
                 thumb_size=file_id.thumbnail_size
             )
-        else:
+        elif file_type == FileType.DOCUMENT:
             location = raw.types.InputDocumentFileLocation(
                 id=file_id.media_id,
                 access_hash=file_id.access_hash,
                 file_reference=file_id.file_reference,
                 thumb_size=file_id.thumbnail_size
             )
+        else:
+            raise ValueError(f"Unsupported file type: {file_type}")
 
         return location
 
     async def yield_file(self, media_msg: Message, offset: int, first_part_cut: int,
-                         last_part_cut: int, part_count: int, chunk_size: int) -> Union[str, None]: #pylint: disable=unsubscriptable-object
+                         last_part_cut: int, part_count: int, chunk_size: int) -> Union[str, None]:
         client = self.main_bot
         data = await self.generate_file_properties(media_msg)
         media_session = await self.generate_media_session(client, media_msg)
@@ -190,7 +161,6 @@ class TGCustomYield:
 
         if isinstance(r, raw.types.upload.File):
             m_file = []
-            # m_file.name = file_name
             while True:
                 chunk = r.bytes
 
