b97f75069b9e4e639018923c61e0ab0439688627
Godofh3ell <66544811+Godofh3ell@users.noreply.github.com>
2024-06-17 15:52:47 +0530

    Update render_template.py

--- web/utils/render_template.py
+++ web/utils/render_template.py
@@ -1,35 +1,13 @@
-from info import BIN_CHANNEL, URL
-from utils import temp
+import urllib.parse
+import aiofiles
+from aiohttp import web
 from web.utils.custom_dl import TGCustomYield
-from utils import get_size
-import urllib.parse
-import secrets
-import mimetypes
-import aiofiles
-import logging
-import aiohttp
-
-import urllib.parse
-import aiofiles
+from utils import temp
+from info import BIN_CHANNEL, URL
 
 PASSWORD = "your_secure_password"  # Define your password here
 
-async def media_watch(message_id, provided_password=None):
-    if provided_password != PASSWORD:
-        return '''
-            <html>
-                <head><title>Password Required</title></head>
-                <body>
-                    <h1>Enter Password to Access the File</h1>
-                    <form method="get">
-                        <input type="password" name="password" required />
-                        <input type="hidden" name="message_id" value="{}" />
-                        <button type="submit">Submit</button>
-                    </form>
-                </body>
-            </html>
-        '''.format(message_id)
-    
+async def media_watch(message_id):
     media_msg = await temp.BOT.get_messages(BIN_CHANNEL, message_id)
     file_properties = await TGCustomYield().generate_file_properties(media_msg)
     file_name, mime_type = file_properties.file_name, file_properties.mime_type
@@ -42,4 +20,30 @@ async def media_watch(message_id, provided_password=None):
     else:
         html = '<h1>This is not a streamable file</h1>'
     return html
-    
+
+async def download_file(message_id, provided_password=None):
+    if provided_password != PASSWORD:
+        return web.Response(text='''
+            <html>
+                <head><title>Password Required</title></head>
+                <body>
+                    <h1>Enter Password to Download the File</h1>
+                    <form method="get">
+                        <input type="password" name="password" required />
+                        <button type="submit">Submit</button>
+                    </form>
+                </body>
+            </html>
+        ''', content_type='text/html')
+
+    media_msg = await temp.BOT.get_messages(BIN_CHANNEL, message_id)
+    file_properties = await TGCustomYield().generate_file_properties(media_msg)
+    file_name = file_properties.file_name
+    file_path = f"downloads/{file_name}"
+
+    if not os.path.exists(file_path):
+        async with aiofiles.open(file_path, 'wb') as f:
+            async for chunk in media_msg.download(file_path):
+                await f.write(chunk)
+
+    return web.FileResponse(file_path)
