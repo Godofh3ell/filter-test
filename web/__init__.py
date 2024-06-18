deafd3b03fdae161a7d779035c7657021298564f
Godofh3ell <66544811+Godofh3ell@users.noreply.github.com>
2024-06-18 13:09:24 +0530

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,15 @@
-# Credit - adarsh-goel
-
 from aiohttp import web
 from web.stream_routes import routes
 
-
+# Create an aiohttp web application instance
 web_app = web.Application()
+
+# Add routes defined in web.stream_routes
 web_app.add_routes(routes)
+
+# Optionally, you can add middleware, error handling, etc., to the web application
+
+# If you want to run the application directly from this file
+if __name__ == "__main__":
+    # Run the web application on localhost and port 8080
+    web.run_app(web_app, host='localhost', port=8080)
